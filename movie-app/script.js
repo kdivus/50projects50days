51698c14b68ecfff1ae414b53112229f906179de
Krešimir Divić <kdivus@hotmail.com>
2021-02-22 13:00:24 +0100

    .

--- movie-app/script.js
+++ movie-app/script.js
@@ -1,13 +1,13 @@
-//const  API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=fb5cce884970cd2cc41bdfc33420d9de&page=1'
-const  API_URL = 'https://api.themoviedb.org/3/discover/movie?550&api_key=fb5cce884970cd2cc41bdfc33420d9de&page=1'
+const  API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=fb5cce884970cd2cc41bdfc33420d9de&page=1'
+//const  API_URL = 'https://api.themoviedb.org/3/discover/movie?550&api_key=fb5cce884970cd2cc41bdfc33420d9de&page=1'
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=fb5cce884970cd2cc41bdfc33420d9de&query="'
+
+const main = document.getElementById('main')
 const form = document.getElementById('form')
 const search = document.getElementById('search')
-const main = document.getElementById('main')
 
-
-//  get initial movies
+// Get initial movies
 getMovies(API_URL)
 
 async function getMovies(url) {
@@ -19,24 +19,23 @@ async function getMovies(url) {
 
 function showMovies(movies) {
     main.innerHTML = ''
+
     movies.forEach((movie) => {
         const { title, poster_path, vote_average, overview } = movie
 
         const movieEl = document.createElement('div')
         movieEl.classList.add('movie')
 
-        movie.innerHTML = `
-        
+        movieEl.innerHTML = `
             <img src="${IMG_PATH + poster_path}" alt="${title}">
             <div class="movie-info">
-                <h3>${title}</h3>
-                <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+          <h3>${title}</h3>
+          <span class="${getClassByRate(vote_average)}">${vote_average}</span>
             </div>
             <div class="overview">
-                <h3>Overview</h3>
-                ${overview}
-            </div>
-       
+          <h3>Overview</h3>
+          ${overview}
+        </div>
         `
         main.appendChild(movieEl)
     })
@@ -45,7 +44,7 @@ function showMovies(movies) {
 function getClassByRate(vote) {
     if(vote >= 8) {
         return 'green'
-    } else if (vote >= 5) {
+    } else if(vote >= 5) {
         return 'orange'
     } else {
         return 'red'
