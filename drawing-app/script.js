ab33b9a261b4e667a279a59de74891275d984528
Shokre <kdivus@hotmail.com>
2021-03-06 14:57:37 +0100

    vars

--- drawing-app/script.js
+++ drawing-app/script.js
@@ -3,6 +3,8 @@ const ctx = canvas.getContext('2d')
 
 let size = 20
 let color = 'black'
+let x
+let y 
 
 function drawCircle(x,y) {
     ctx.beginPath()
@@ -14,6 +16,9 @@ function drawCircle(x,y) {
 function drawLine(x1, y1, x2, y2) {
     ctx.beginPath()
     ctx.moveTo(x1, y1)
+    ctx.lineTo(x2, y2)
+    ctx.strokeStyle = color 
+    ctx.lineWidth = size
+    ctx.stroke()
 }
 
-drawCircle(100, 200)
