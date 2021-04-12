fcea0e043c7631ebb4fa107cc1dc870ec01439ba
Shokre <kdivus@hotmail.com>
2021-04-12 18:31:18 +0200

    starting with js

---
+++ double-heart-click/script.js
@@ -0,0 +1,16 @@
+const loveMe = document.querySelector('.loveMe')
+const times = document.querySelector('#times')
+
+let clickTime = 0
+
+loveMe.addEventListener('click', (e) => {
+    if(clickTime === 0){
+        clickTime = new Date().getTime()
+    } else {
+        if((new Date().getTime() - clickTime) < 800) {
+            console.log(123)
+        } else {
+            clickTime = new Date().getTime()
+        }
+    }
+})
